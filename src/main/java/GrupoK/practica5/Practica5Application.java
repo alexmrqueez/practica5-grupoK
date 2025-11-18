74f0baded705290cf61b3a8350b9d426aa501196
alexmrqueez <alexmarquezmalia@gmail.com>
2025-11-18 19:04:38 +0100

    Proyecto inicial Spring Boot

---
+++ src/main/java/GrupoK/practica5/Practica5Application.java
@@ -0,0 +1,13 @@
+package GrupoK.practica5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practica5Application.class, args);
+	}
+
+}
