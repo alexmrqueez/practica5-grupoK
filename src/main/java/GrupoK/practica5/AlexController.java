e357c51c92f55929eb59df46122b590bf93b5343
alexmrqueez <alexmarquezmalia@gmail.com>
2025-11-20 18:29:02 +0100

    Añadir AlexController con Hello World

---
+++ src/main/java/GrupoK/practica5/AlexController.java
@@ -0,0 +1,13 @@
+package GrupoK.practica5;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AlexController {
+
+    @GetMapping("/alex")
+    public String saludo() {
+        return "¡hello world desde Alex!";
+    }
+}
